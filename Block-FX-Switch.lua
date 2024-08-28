d88aa2926b97e6308057970ba74604235f31265f
The Dumb <info@hemisphera.com>
2024-08-28 20:28:35 +0200

    Fix

--- Block-FX-Switch.lua
+++ Block-FX-Switch.lua
@@ -11,17 +11,15 @@ function MuteFXBlock()
     trackStart = 20
   end
 
-    --reaper.ShowConsoleMsg(tostring(selectedTrackNumber) .. '\n')
-  
   local trackEnd = trackStart + 10
   for i = trackStart, trackEnd - 1 do
     local track = reaper.GetTrack(0, i - 1)
     
-    local peak0 = reaper.RPR_Track_GetPeakInfo(track, 0)
-    local peak1 = reaper.RPR_Track_GetPeakInfo(track, 1)
+    local peak0 = reaper.Track_GetPeakInfo(track, 0)
+    local peak1 = reaper.Track_GetPeakInfo(track, 1)
     local isIdle = (peak0 <= 0.1) and (peak1 <= 0.1)
     if (isIdle) then
-      reaper.SetMediaTrackInfo_Value(track, 'B_MUTE ', 1.0)
+      reaper.SetMediaTrackInfo_Value(track, 'B_MUTE', 1)
     end
 
     local _, name = reaper.GetTrackName(track)
@@ -32,7 +30,7 @@ function MuteFXBlock()
   end
   local track = reaper.GetSelectedTrack(0, 0)
   reaper.SetMediaTrackInfo_Value(track, 'I_RECARM', 1.0)
-  reaper.SetMediaTrackInfo_Value(track, 'B_MUTE ', 0.0)
+  reaper.SetMediaTrackInfo_Value(track, 'B_MUTE', 0)
 end
 
 MuteFXBlock();
